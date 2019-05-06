d7634ef52c897a57fa85e446f512fefeb18876a8
Vicente Baixauli <vibaiher@gmail.com>
2019-05-06 11:37:20 +0200

    Send contact form to Mattermost

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -1,26 +1,29 @@
 function callToAction(form) {
-    let data = {
+    var data = {
         'name': form.firstname.value,
         'email': form.email.value,
         'comments': form.comments.value
-    }
-    payload={
+    };
+    var payload = {
         "text": data.name + " (" + data.email + ") ha enviado un comentario:\n\n" + data.comments,
         "username": 'devscola',
         "icon_url": 'http://www.devscola.org/assets/images/Logotipo-01.png'
-    }
-  
-    this.post(JSON.stringify(payload))
-    this.reset(form)
-    return false
-  }
-  function post(data) {
+    };
+
+    this.post(JSON.stringify(payload));
+    this.reset(form);
+
+    return false;
+}
+
+function post(data) {
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://hooks.slack.com/services/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');
-    xhr.send(data)
-  }
-  function reset(form) {
-      form.firstname.value = ''
-      form.email.value = ''
-      form.comments.value = ''
-  }
+    xhr.open('POST', 'https://chat.devscola.org/hooks/g7a9zpkp6ig1jfjjz37qq7xhee');
+    xhr.send(data);
+}
+
+function reset(form) {
+    form.firstname.value = '';
+    form.email.value = '';
+    form.comments.value = '';
+}
