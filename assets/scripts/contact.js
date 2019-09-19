415af10b982e5c177c7b5c16b2282b4f37ca5435
Vicente Baixauli <vibaiher@protonmail.com>
2019-09-19 23:28:02 +0200

    Mostrar feedback al enviar el mensaje de contacto

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -1,29 +1,33 @@
 function callToAction(form) {
-    var data = {
-        'name': form.firstname.value,
-        'email': form.email.value,
-        'comments': form.comments.value
-    };
-    var payload = {
-        "text": data.name + " (" + data.email + ") ha enviado un comentario:\n\n" + data.comments,
-        "username": 'devscola',
-        "icon_url": 'http://www.devscola.org/assets/images/Logotipo-01.png'
-    };
+  var data = {
+    'name': form.firstname.value,
+    'email': form.email.value,
+    'comments': form.comments.value
+  };
+  var payload = {
+    "text": data.name + " (" + data.email + ") ha enviado un comentario:\n\n" + data.comments,
+    "username": 'devscola',
+    "icon_url": 'http://www.devscola.org/assets/images/Logotipo-01.png'
+  };
 
-    this.post(JSON.stringify(payload));
-    this.reset(form);
+  this.post(JSON.stringify(payload));
+  this.reset(form);
 
-    return false;
+  return false;
 }
 
 function post(data) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://chat.devscola.org/hooks/g7a9zpkp6ig1jfjjz37qq7xhee');
-    xhr.send(data);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://chat.devscola.org/hooks/g7a9zpkp6ig1jfjjz37qq7xhee');
+  xhr.send(data);
 }
 
 function reset(form) {
-    form.firstname.value = '';
-    form.email.value = '';
-    form.comments.value = '';
+  form.firstname.value = '';
+  form.email.value = '';
+  form.comments.value = '';
+  document.querySelector("#contact-feedback").style = "display: inline-block;"
+  setTimeout(function () {
+    document.querySelector("#contact-feedback").style = "display: none;"
+  }, 4000)
 }
